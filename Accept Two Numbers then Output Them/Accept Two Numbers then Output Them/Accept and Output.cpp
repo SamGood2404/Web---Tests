7f1c97f1525b006320905fcc1dc759cc55c68d81
SamGood2404 <38388332+SamGood2404@users.noreply.github.com>
2018-05-20 20:34:18 +1200

    Accept Two Numbers & Output
    SG

---
+++ Accept Two Numbers then Output Them/Accept Two Numbers then Output Them/Accept and Output.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int Num1;
+	int Num2;
+
+	cout << "Please enter two numbers..." << endl;
+	cout << endl;
+
+	cin >> Num1;
+	cin >> Num2;
+
+	cout << "Number 1 was: " << Num1 << endl;
+	cout << "Number 2 was: " << Num2 << endl;
+
+	int iTemp;
+	cin >> iTemp;
+
+	return(0);
+}
